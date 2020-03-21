170e4c0071e00d715dd7521155f38c26a49ee83c
Singlea-lyh <chinese_lyh@163.com>
2020-03-21 21:53:30 +0800

    LeetCode-6

---
+++ C/6.zig-zag-conversion.c
@@ -0,0 +1,64 @@
+/*
+ * @lc app=leetcode id=6 lang=c
+ *
+ * [6] ZigZag Conversion
+ *
+ * https://leetcode.com/problems/zigzag-conversion/description/
+ *
+ * algorithms
+ * Medium (34.90%)
+ * Likes:    1448
+ * Dislikes: 4173
+ * Total Accepted:    420.2K
+ * Total Submissions: 1.2M
+ * Testcase Example:  '"PAYPALISHIRING"\n3'
+ *
+ * The string "PAYPALISHIRING" is written in a zigzag pattern on a given number
+ * of rows like this: (you may want to display this pattern in a fixed font for
+ * better legibility)
+ * 
+ * 
+ * P   A   H   N
+ * A P L S I I G
+ * Y   I   R
+ * 
+ * 
+ * And then read line by line: "PAHNAPLSIIGYIR"
+ * 
+ * Write the code that will take a string and make this conversion given a
+ * number of rows:
+ * 
+ * 
+ * string convert(string s, int numRows);
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: s = "PAYPALISHIRING", numRows = 3
+ * Output: "PAHNAPLSIIGYIR"
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: s = "PAYPALISHIRING", numRows = 4
+ * Output: "PINALSIGYAHRPI"
+ * Explanation:
+ * 
+ * P     I    N
+ * A   L S  I G
+ * Y A   H R
+ * P     I
+ * 
+ */
+
+// @lc code=start
+
+
+char * convert(char * s, int numRows){
+
+}
+
+
+// @lc code=end
+
