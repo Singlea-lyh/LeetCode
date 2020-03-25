76152cf13c57fe6992e32296f543b021581d8599
Singlea-lyh <chinese_lyh@163.com>
2020-03-25 21:17:15 +0800

    LeetCode-10  Add Title

---
+++ C/10.regular-expression-matching.c
@@ -0,0 +1,120 @@
+/*
+ * @lc app=leetcode id=10 lang=c
+ *
+ * [10] Regular Expression Matching
+ *
+ * https://leetcode.com/problems/regular-expression-matching/description/
+ *
+ * algorithms
+ * Hard (26.24%)
+ * Likes:    3689
+ * Dislikes: 644
+ * Total Accepted:    395.3K
+ * Total Submissions: 1.5M
+ * Testcase Example:  '"aa"\n"a"'
+ *
+ * Given an input string (s) and a pattern (p), implement regular expression
+ * matching with support for '.' and '*'.
+ * 
+ * 
+ * '.' Matches any single character.
+ * '*' Matches zero or more of the preceding element.
+ * 
+ * 
+ * The matching should cover the entire input string (not partial).
+ * 
+ * Note:
+ * 
+ * 
+ * s could be empty and contains only lowercase letters a-z.
+ * p could be empty and contains only lowercase letters a-z, and characters
+ * like . or *.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input:
+ * s = "aa"
+ * p = "a"
+ * Output: false
+ * Explanation: "a" does not match the entire string "aa".
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input:
+ * s = "aa"
+ * p = "a*"
+ * Output: true
+ * Explanation: '*' means zero or more of the preceding element, 'a'.
+ * Therefore, by repeating 'a' once, it becomes "aa".
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input:
+ * s = "ab"
+ * p = ".*"
+ * Output: true
+ * Explanation: ".*" means "zero or more (*) of any character (.)".
+ * 
+ * 
+ * Example 4:
+ * 
+ * 
+ * Input:
+ * s = "aab"
+ * p = "c*a*b"
+ * Output: true
+ * Explanation: c can be repeated 0 times, a can be repeated 1 time. Therefore,
+ * it matches "aab".
+ * 
+ * 
+ * Example 5:
+ * 
+ * 
+ * Input:
+ * s = "mississippi"
+ * p = "mis*is*p*."
+ * Output: false
+ * 
+ * 
+ */
+
+// @lc code=start
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+typedef int bool;
+#define true 1
+#define false 0
+
+
+bool isMatch(char * s, char * p){
+
+}
+
+
+int main(){
+    char cStr[] = "";
+    char cPattern[] = "";
+
+    bool bIsReturn = isMatch(cStr, cPattern);
+
+    int iReturn1 = (bIsReturn) ? 1 : 0;
+    
+
+    printf("%d\n", iReturn1);
+
+    return 0;    
+}
+
+
+// @lc code=end
+
