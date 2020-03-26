f111e8bef0e199e0e52c74fdd47bd0ae11807e80
Singlea-lyh <chinese_lyh@163.com>
2020-03-26 22:59:51 +0800

    LeetCode-10

--- C/10.regular-expression-matching.c
+++ C/10.regular-expression-matching.c
@@ -96,14 +96,133 @@ typedef int bool;
 #define false 0
 
 
-bool isMatch(char * s, char * p){
+// bool isMatch(char * s, char * p){
+//     int iStrLen = 0;
+//     int iPatternLen = 0;
+//     int iStr = 0, iPat = 0;
+    
+//     iStrLen = strlen(s);
+//     iPatternLen = strlen(p);
 
+//     if(iStrLen == 0 || iPatternLen == 0){
+//         return false;
+//     }
+
+
+//     int i = 0, j = 0;
+
+//     while(i < iPatternLen && j < iStrLen){
+//         char cPatTemp = p[i];
+//         char cStrTemp = s[j];
+
+//         if(cStrTemp < 'a' || cStrTemp > 'z'){
+//             return false;
+//         }
+
+//         if((cPatTemp < 'a' || cPatTemp > 'z') && (cPatTemp != '.' && cPatTemp != '*')){
+//             return false;
+//         }
+
+//         if(cPatTemp == '.'){
+//             ++i;
+//             ++j;
+//         }
+//         else if(cPatTemp == '*'){
+//             char cStr[] = "aaa";
+//             char cPattern[] = "aa*c*c*a";
+//             if(j + 1 < iStrLen && i + 1 < iPatternLen && s[j + 1] != p[i + 1]){
+//                 if((p[i - 1] == '.' || p[i - 1] == cStrTemp) && s[j + 1] == cStrTemp){
+//                     ++j;
+//                 }
+//                 else if((p[i - 1] == '.' || p[i - 1] == cStrTemp) && s[j + 1] != cStrTemp){
+//                     ++i;
+//                     ++j;
+//                 }      
+//                 else{
+//                     return false;
+//                 }
+//             }
+//             else if(j + 1 < iStrLen && i + 1 < iPatternLen && s[j + 1] == p[i + 1]){
+//                 if(p[i - 1] == '.' || p[i - 1] == cStrTemp){
+//                     ++j;
+//                     ++i;
+//                 }      
+//                 else{
+//                     return false;
+//                 }
+//             }
+//             else if(i + 1 < iPatternLen && cStrTemp == p[i+1]){
+//                 i += 2;
+//                 ++j;
+//             }
+//             else{
+//                 if(i + 2 < iPatternLen && p[i + 2] == '*'){
+//                     if(i + 3 < iPatternLen && p[i + 3] == cStrTemp){
+//                         i = i +4;
+//                         ++j;
+//                     }
+//                     else{
+//                         i += 2;
+//                     }
+
+//                 } 
+//                 else if(p[i - 1] == '.' || p[i - 1] == cStrTemp){
+//                     ++j;
+//                 }     
+//                 else{
+//                     return false;
+//                 }
+//             }
+//         }
+//         else if(cPatTemp == cStrTemp){
+//             ++i;
+//             ++j;
+//         }
+//         else if(cPatTemp != cStrTemp){
+//             if(p[i + 1] == '*'){
+//                 i += 2;
+//             }
+//             else{
+//                 return false;       
+//             }    
+//         }
+//         else{
+//             return false;
+//         }
+//     }
+    
+//     if(i < iPatternLen - 1 && j <= iStrLen){
+//         return false;
+//     }
+//     else if(j == iStrLen){
+//         if((i == iPatternLen - 1 && p[i] == '*') || (iPatternLen == i && p[i - 1] != '*')){
+//             return true;
+//         }
+//         else{
+//             return false;
+//         }
+//     }
+//     else{
+//         return false;
+//     }
+    
+//     return false;
+// }
+
+bool isMatch(char * s, char * p){
+    if (!*p) return !*s;
+    bool bIsFirstMatch = *s && (*s == *p || *p == '.');
+    if (*(p+1) == '*') {
+        return isMatch(s, p+2) || (bIsFirstMatch && isMatch(++s, p));
+    }
+    else {
+        return bIsFirstMatch && isMatch(++s, ++p);
+    }
 }
 
-
 int main(){
-    char cStr[] = "";
-    char cPattern[] = "";
+    char cStr[] = "aaca";
+    char cPattern[] = "ab*a*c*a";
 
     bool bIsReturn = isMatch(cStr, cPattern);
 
