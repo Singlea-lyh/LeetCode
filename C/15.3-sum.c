9897aaddecee2b7621932b5f4ea3d81c4674fbeb
Singlea-lyh <chinese_lyh@163.com>
2020-04-03 15:47:11 +0800

    LeetCode-15 Change

--- C/15.3-sum.c
+++ C/15.3-sum.c
@@ -61,8 +61,6 @@ void QuickSort(int* aiNum, int iLeft, int iRight){
         return;
 
     }
-
-
     while(iTempLeft != iTempRight){
         while(aiNum[iTempRight] >= iTemp && iTempRight > iTempLeft){
             --iTempRight;
@@ -103,43 +101,62 @@ int* ArraySort(int *nums, int numsSize){
 int** threeSum(int* nums, int numsSize, int* returnSize, int** returnColumnSizes){
     int **ppaiReturnArray = NULL;
     int iReturnSize = 0;
-    int* iSortNums = ArraySort(nums, numsSize);
+    int* iSortNums = NULL;
 
-    // for(int i = 0; i < 6; ++i){
-    //     printf("%d , " , iSortNums[i]);
-    // }
-    // printf("\n");
+
 
     if(numsSize < 3){
+        *returnSize = 0;
+        *returnColumnSizes = NULL;
         return NULL;
     }
+
+    iSortNums = ArraySort(nums, numsSize);
+
+    for(int i = 0; i < numsSize; ++i){
+        printf("%d , " , iSortNums[i]);
+    }
+    printf("\n");
 
     ppaiReturnArray = (int**) malloc(sizeof(int*) * numsSize * 6);
 
     
-    for(int i = 0; i < numsSize; ){
+    for(int i = 0; i < numsSize - 2; ){
         int iBase = iSortNums[i];
         int iLeft = i + 1, iRight = numsSize - 1;
 
         while(iLeft < iRight){
-            while(iBase + iSortNums[iLeft] + iSortNums[iRight] > 0 && iLeft < iRight){
-                --iRight;
+            while(iBase + iSortNums[iLeft] + iSortNums[iRight] != 0 && iLeft < iRight){
+                if(iBase + iSortNums[iLeft] + iSortNums[iRight] > 0){
+                    --iRight;
+                }
+                else if(iBase + iSortNums[iLeft] + iSortNums[iRight] < 0){
+                    ++iLeft;
+                }
+                
             }
-            while(iBase + iSortNums[iLeft] + iSortNums[iRight] < 0 && iLeft < iRight){
-                ++iLeft;
-            }
-            if(iBase + iSortNums[iLeft] + iSortNums[iRight] == 0 && iLeft != iRight){
+            // while(iBase + iSortNums[iLeft] + iSortNums[iRight] < 0 && iLeft < iRight){
+            //     ++iLeft;
+            // }
+            if(iBase + iSortNums[iLeft] + iSortNums[iRight] == 0 && iLeft < iRight){
                 ppaiReturnArray[iReturnSize] = (int*)malloc(sizeof(int) * 3);
                 ppaiReturnArray[iReturnSize][0] = iBase;
                 ppaiReturnArray[iReturnSize][1] = iSortNums[iLeft];
                 ppaiReturnArray[iReturnSize][2] = iSortNums[iRight];
                 iReturnSize++;
             }
-            ++iLeft;
-            --iRight;
+            if(iRight > iLeft){
+                int iLeftVaule = iSortNums[iLeft++], iRightVaule = iSortNums[iRight--];
+                while(iLeft < iRight && iLeftVaule == iSortNums[iLeft]) {
+                    ++iLeft;
+                };
+                while(iLeft < iRight && iRightVaule == iSortNums[iRight]){
+                    --iRight;
+                }
+            }
         }
 
-        while(iSortNums[++i] == iBase);
+        while(++i < (numsSize - 1) && iSortNums[i] == iBase);
     }
 
     // *returnSize = numsSize;
@@ -157,13 +174,14 @@ int** threeSum(int* nums, int numsSize, int* returnSize, int** returnColumnSizes
 }
 
 int main(){
-    int a[] = {-1, 0, 1, 2, -1, -4};
+    int a[] = {-4,-1,-4,0,2,-2,-4,-3,2,-3,2,3,3,-4};
+
     int *iReturnSize = (int*)malloc(sizeof(int));
     int *iReturnColumnSizes = (int*)malloc(sizeof(int));
 
-    int **pp3Sum = threeSum(a, 6, iReturnSize, &iReturnColumnSizes);
+    int **pp3Sum = threeSum(a, 14, iReturnSize, &iReturnColumnSizes);
 
-    for(int i = 0; i < *iReturnColumnSizes; ++i){
+    for(int i = 0; i < *iReturnSize; ++i){
         for(int j = 0; j < 3; ++j){
             printf("%d  ", pp3Sum[i][j]);
         }
