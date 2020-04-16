e65f89cb773de25a424d7379a7cff26377ef5f07
Singlea-lyh <chinese_lyh@163.com>
2020-04-16 20:01:32 +0800

    LeetCode-22

--- C/22.generate-parentheses.c
+++ C/22.generate-parentheses.c
@@ -29,7 +29,7 @@
  * ⁠ "()(())",
  * ⁠ "()()()"
  * ]
- * 
+ *  
  */
 
 // @lc code=start
@@ -44,15 +44,56 @@
 #include <math.h>
 #include <string.h>
 
+void dfsgenerate(int iParentheses, int iLeft, int iRight, int iIndex, int *piReturnSize, char* pacItem, char** ppacReturn){
+    if(iLeft == 0 && iRight == 0){
+        ppacReturn[*piReturnSize] = (char*) malloc(sizeof(char) * (2 * iParentheses + 1));
+        sprintf( ppacReturn[*piReturnSize], "%s\0", pacItem);
+        (*piReturnSize)++;
+        return;
+    }
+
+    if(iLeft > iRight){
+        return;
+    }
+
+    if(iLeft > 0){
+        pacItem[iIndex] = '(';
+        dfsgenerate(iParentheses ,iLeft - 1, iRight, iIndex + 1, piReturnSize, pacItem, ppacReturn);  
+    }
+
+    if(iRight > 0){
+        pacItem[iIndex] = ')';        
+        dfsgenerate(iParentheses, iLeft, iRight - 1, iIndex + 1, piReturnSize, pacItem, ppacReturn);
+
+    }
+}
+
 
 char ** generateParenthesis(int n, int* returnSize){
+    char** ppacReturn = (char**)malloc(sizeof(char*) * 5000);
+    char* pacItem = (char*) malloc(sizeof(char) * (2 * n + 1));
+    int iReturnSize = 0;
+    int iLeft = n, iRight = n;
+    int iIndex = 0;
 
+    if(n == 0){
+        *returnSize = iReturnSize;
+        return NULL;
+    }
+
+    memset(pacItem, 0, 2 * n + 1);
+
+    dfsgenerate(n, iLeft, iRight, iIndex, &iReturnSize, pacItem, ppacReturn);
+
+    *returnSize = iReturnSize;
+
+    return ppacReturn;
 }
 
 
 int main(){
     int iReturnSize = 0;
-    int iN = 0;
+    int iN = 8;
     char** ppacReturn = NULL;
     
     ppacReturn = generateParenthesis(iN, &iReturnSize);
@@ -67,8 +108,6 @@ int main(){
 
     return 0;    
 }
-
-
 
 // @lc code=end
 
