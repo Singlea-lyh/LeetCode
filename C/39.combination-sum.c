5051e0d385ee917aa5a6ff014b47ae7b6a9d41b2
Singlea-lyh <chinese_lyh@163.com>
2020-05-18 22:08:36 +0800

    LeetCode-39

--- C/39.combination-sum.c
+++ C/39.combination-sum.c
@@ -66,26 +66,124 @@
 #include <string.h>
 #include <math.h>
 
-int** combinationSum(int* candidates, int candidatesSize, int target, int* returnSize, int** returnColumnSizes){
+#define MAX_LEN 256
+
+void QuickSort(int* piCandidates, int iStart , int iEnd){
+    int iLeft = iStart, iRight = iEnd;
+    int iBase = piCandidates[iStart];
+
+    if(iLeft > iRight){
+        return;
+    }
     
+    while(iLeft != iRight){
+        while(iLeft < iRight && piCandidates[iRight] >= iBase) --iRight;
+        while(iLeft < iRight && piCandidates[iLeft] <= iBase) ++iLeft;
+
+        if(iLeft < iRight){
+            piCandidates[iLeft] = piCandidates[iLeft] ^ piCandidates[iRight];
+            piCandidates[iRight] = piCandidates[iLeft] ^ piCandidates[iRight];
+            piCandidates[iLeft] = piCandidates[iLeft] ^ piCandidates[iRight];
+        }
+    }
+
+    piCandidates[iStart] = piCandidates[iLeft];
+    piCandidates[iLeft] = iBase;
+
+    QuickSort(piCandidates, iStart, iLeft - 1);
+    QuickSort(piCandidates, iLeft + 1, iEnd);
+
+    return;
+}
+
+void Sort(int* piCandidates, int iCandidatesSize){
+    QuickSort(piCandidates, 0, iCandidatesSize - 1);
+
+    return;
+}
+
+int backtrace(int* piCandidates, int iCandidatesSize, int iTarget, int** ppReturn, int* piReturnSize , int* piReturnCol,
+              int iRetPos, int iIndex ){
+  
+    if(iTarget == 0){
+        return 0;
+    }
+    if(iTarget < 0){
+        return -1;
+    }
+
+    for(int i = iIndex; i < iCandidatesSize; ++i){
+        ppReturn[*piReturnSize][iRetPos] = piCandidates[i];
+
+        int iTemp = backtrace(piCandidates, iCandidatesSize, iTarget - piCandidates[i], ppReturn, 
+                            piReturnSize, piReturnCol, iRetPos + 1, i);
+        
+        if(iTemp == 0){
+            piReturnCol[*piReturnSize] = iRetPos + 1;
+            memcpy(ppReturn[*piReturnSize + 1] , ppReturn[*piReturnSize], sizeof(int) * (iRetPos + 1));
+            (*piReturnSize)++;
+            return 1;
+        }
+        if(iTemp == -1){
+            return 1;
+        }
+    }
+
+    return 1;
+}
+
+int** combinationSum(int* candidates, int candidatesSize, int target, int* returnSize, int** returnColumnSizes){
+    int** ppReturn = NULL;
+    int* piReturnCol = NULL;
+    int iReturnSize = 0;
+
+    if(candidatesSize < 1){
+        *returnSize = 0;
+        return ppReturn;
+    }
+
+    ppReturn = (int**)malloc(sizeof(int*) * MAX_LEN);
+    piReturnCol = (int*)malloc(sizeof(int) * MAX_LEN);
+    memset(piReturnCol, 0, sizeof(int)*MAX_LEN);
+
+    for(int i = 0; i < MAX_LEN; ++i){
+        ppReturn[i] = (int*)malloc(sizeof(int) * MAX_LEN);
+        memset(ppReturn[i], 0, sizeof(int) * MAX_LEN);
+    }
+
+    Sort(candidates, candidatesSize);
 
 
+    for(int i = 0; i < candidatesSize; ++i){
+        printf("%d, ", candidates[i]);
+    }
+    printf("\n");
+
+    backtrace(candidates, candidatesSize, target, ppReturn, &iReturnSize, piReturnCol, 0, 0);
+
+    for(int i = iReturnSize; i < MAX_LEN; ++i){
+        free(ppReturn[i]);
+    }
+
+    *returnSize = iReturnSize;
+    *returnColumnSizes = piReturnCol;
+    return ppReturn;
 }
 
 int main(){
-    int aiCandidates[] = {};
-    int iCandidatesSize = 0;
-    int iTarget = 0;
+    int aiCandidates[] = {2,3,6,7};
+    int iCandidatesSize = 4;
+    int iTarget = 6;
     int iReturnSize = 0;
-    int** ppReturnColumnsSize = NULL;
+    int* ppReturnColumnsSize = NULL;
     int** ppReturn = NULL;
 
-    ppReturn = combinationSum(aiCandidates, iCandidatesSize, iTarget, &iReturnSize, ppReturnColumnsSize);
+    ppReturn = combinationSum(aiCandidates, iCandidatesSize, iTarget, &iReturnSize, &ppReturnColumnsSize);
 
     printf("[\n");
     for(int i = 0; i < iReturnSize; ++i){
         printf("[");        
-        for(int j = 0; j < ppReturnColumnsSize[i]; ++j){
+        for(int j = 0; j < (ppReturnColumnsSize)[i]; ++j){
             printf("%d, ", ppReturn[i][j]);
         }
         printf("]\n");
