2558b9cf0dc0e8fda8068f3f528f4c60514d5416
Singlea-lyh <chinese_lyh@163.com>
2020-05-26 23:13:03 +0800

    LeetCode-43

--- C/43.multiply-strings.c
+++ C/43.multiply-strings.c
@@ -52,9 +52,18 @@
 char * multiply(char * num1, char * num2){
     int iNumLen1 = strlen(num1);
     int iNumLen2 = strlen(num2);
+    int iMulLen = 0;
+    char* pcReturn = NULL;
+    char* pcReturnTemp = (char*) malloc(sizeof(char) * (iNumLen1 + iNumLen2 + 2));
+    memset(pcReturnTemp, 0, sizeof(char) * (iNumLen1 + iNumLen2 + 1));
+
+    pcReturn = (char*)malloc(sizeof(char) * (iNumLen1 + iNumLen2 + 2));
+    memset(pcReturn, 0, sizeof(char) * (iNumLen1 + iNumLen2 + 1));
 
     if((iNumLen1 == 1 && num1[0] == '0') || (iNumLen2 == 1 && num2[0] == '0')){
-        return 0;
+        free(pcReturn);
+        pcReturnTemp[0] = (char) ('0' + 0);
+        return pcReturnTemp;
     }
     if(iNumLen1 == 1 && num1[0] == '1'){
         return num2;
@@ -62,22 +71,57 @@ char * multiply(char * num1, char * num2){
     else if(iNumLen2 == 1 && num2[0] == '1'){
         return num1;
     }
-    
-    
 
+    for(int i = iNumLen2 - 1; i >= 0; --i){
+        int iCarry = 0;
+        int iBase = num2[i] - '0';
+        int iLocal = 0;
+        char* pcTemp = (char*)malloc(sizeof(char) * (iNumLen1 + iNumLen2 + 2));
+        memset(pcTemp, 0, sizeof(char) * (iNumLen1 + iNumLen2 + 2));
+        for(iLocal = 0; iLocal < (iNumLen2 - 1 - i); ++iLocal);
 
+        for(int j = iNumLen1 - 1; j >= 0; --j){
+            int iTemp = num1[j] - '0';
+            int iMulTemp = (iBase * iTemp + iCarry) % 10;
+            pcTemp[iLocal++] = (char)('0' + iMulTemp);
+            iCarry = (iBase * iTemp + iCarry) / 10;
+        }
+        if(iCarry != 0){
+            pcTemp[iLocal++] =  (char)('0' + iCarry);
+        }
+
+        int  iAddCarry = 0;
+        for(int l = 0; l < iLocal; ++l){
+            int iTemp = (pcTemp[l] == 0) ? 0 : (pcTemp[l] - '0');
+            int iReturn = (pcReturn[l] == 0) ? 0 : (pcReturn[l] - '0');
+            int iAddTemp = ( iTemp+  iReturn + iAddCarry) % 10;
+            iAddCarry = (iTemp+  iReturn + iAddCarry) / 10;
+            pcReturn[l] = (char)('0' + iAddTemp);
+        }
+        if(iAddCarry != 0){
+            pcReturn[iLocal++] = (char)('0' + iAddCarry);
+        }
+        iMulLen = (iLocal > iMulLen) ? iLocal : iMulLen;
+    }
+
+    for(int i = 0; i < iMulLen; ++i){
+        pcReturnTemp[i] = pcReturn[iMulLen - 1 - i];
+    }
+    free(pcReturn);
+
+    return pcReturnTemp;
 }
 
 int main(){
-    char* pcNum1 = "2";
-    char* pcNum2 = "3";
+    char* pcNum1 = "140";
+    char* pcNum2 = "721";
     char* pcReturn = NULL;
 
     pcReturn = multiply(pcNum1, pcNum2);
 
     printf("%s\n", pcReturn);
 
-    free(pcReturn);
+    // free(pcReturn);
 
     return 0;
 }
