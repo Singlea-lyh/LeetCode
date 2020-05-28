3bd5ee4732e5c96dd3ad27ee154e1ce0ec07ec7f
Singlea-lyh <chinese_lyh@163.com>
2020-05-28 23:29:43 +0800

    LeetCode-44

--- C/44.wildcard-matching.c
+++ C/44.wildcard-matching.c
@@ -94,20 +94,88 @@ typedef int bool;
 #define false 0
 #define true 1
 
+// bool isMatch(char * s, char * p){
+//     bool bIsReturn = false;
+//     int iSlen = strlen(s);
+//     int iPatternLen = strlen(p);
+//     bool** ppPattern = NULL;
+
+//     ppPattern = (bool**)malloc(sizeof(bool*) * (iSlen + 1));
+//     for(int i = 0; i <= iSlen; ++i){
+//         ppPattern[i] = (bool*)malloc(sizeof(bool) * (iPatternLen + 1));
+//     }
+
+//     for(int i = 0; i <= iSlen; ++i){
+//         for(int j = 0; j <= iPatternLen; ++j){
+//             ppPattern[i][j] = false;
+//         }
+//     }
+
+//      ppPattern[0][0] = true;
+
+//     for(int i = 1; i <= iPatternLen; ++i){
+//         ppPattern[0][i] = (ppPattern[0][i - 1] && (p[i - 1] == '*'));
+//     }
+
+//     for(int i = 1; i <= iSlen; ++i){
+//         for(int j = 1; j <= iPatternLen; ++j){
+//             if(s[i - 1] == p[j - 1] || p[j - 1] == '?'){
+//                 ppPattern[i][j] = ppPattern[i - 1][j - 1];
+//             }
+//             else if(p[j - 1] == '*'){
+//                 ppPattern[i][j] = ppPattern[i][j - 1] || ppPattern[i - 1][j];
+//             }
+//         }
+//     }
+
+//     // for(int i = 0; i <= iSlen; ++i){
+//     //     for(int j = 0; j <= iPatternLen; ++j){
+//     //         printf("%d, ", ppPattern[i][j]);
+//     //     }
+//     //     printf("\n");
+//     // }
+
+//     bIsReturn = ppPattern[iSlen][iPatternLen];
+//     for(int i = 0; i < iSlen; ++i){
+//         free(ppPattern[i]);
+//     }
+//     free(ppPattern);
+
+//     return bIsReturn;  
+// }
+
 bool isMatch(char * s, char * p){
     bool bIsReturn = false;
     int iSlen = strlen(s);
     int iPatternLen = strlen(p);
+    int iSStar = -1, iPStar = -1;
+    int i = 0, j = 0;
+    while(i < iSlen){
+        if(j < iPatternLen && s[i] == p[j] || p[j] == '?'){
+            ++i;
+            ++j;
+        }
+        else if(j < iPatternLen && p[j] == '*'){
+            iSStar = i;
+            iPStar = j++;
+        }
+        else if(iSStar >= 0){
+            i = ++iSStar;
+            j = iPStar + 1;
+        }
+        else{
+            return false;
+        }
+    } 
+    while(j < iPatternLen && p[j] == '*') ++j;
 
-
-
-    return bIsReturn;    
+    return (j == iPatternLen);  
 }
 
 int main(){
     bool bIsMatch = false;
-    char* pcSource = "aa";
-    char* pcPattern = "a";
+    char* pcSource = "";
+    char* pcPattern = "*";
 
     bIsMatch = isMatch(pcSource, pcPattern);
 
