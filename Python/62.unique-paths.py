dc8d76c42abc1aeecafe290e92c798c75cccaa01
Singlea-lyh <chinese_lyh@163.com>
2020-06-24 23:51:07 +0800

    LeetCode-62

--- Python/62.unique-paths.py
+++ Python/62.unique-paths.py
@@ -58,10 +58,107 @@
 
 # @lc code=start
 class Solution:
-    def uniquePaths(self, m: int, n: int) -> int:
-        ret = 0
+    # def uniquePaths(self, m: int, n: int) -> int:
+        # def recurrence(row, rows, col, cols, temp, ret):
+        #     if row == rows and col == cols:
+        #         result.append(temp)
+        #         ret += 1
+        #         return ret
+            # elif row == rows:
+            #     tempcol = col + 1
+            #     while col != cols:
+            #         temp.append("Right")
+            #         col += 1
+            #     result.append(temp)
+            #     while col != tempcol:
+            #         del temp[-1]
+            #         col -= 1
+            #     ret += 1
+            #     return ret
+            # elif col == cols:
+            #     temprow = row + 1
+            #     while row != rows:
+            #         temp.append("Down")
+            #         row += 1
+            #     while row != temprow:
+            #         del temp[-1]
+            #         row -= 1
+            #     result.append(temp)
+            #     ret += 1
+            #     return ret
+            # else:
+            #     if row == rows:
+            #         temp.append("Right")
+            #         ret = recurrence(row, rows, col + 1, cols, temp, ret)
+            #     elif col == cols:
+            #         temp.append("Down")
+            #         ret = recurrence(row + 1, rows, col, cols, temp, ret)
+            #     else:
+            #         for i in range(2):
+            #             if i == 0:
+            #                 temp.append("Right")
+            #                 ret = recurrence(row, rows, col + 1, cols, temp, ret)
+            #             else:
+            #                 temp.append("Down")
+            #                 ret = recurrence(row + 1, rows, col, cols, temp, ret)
+            #     del temp[-1]
 
-        return ret
+
+
+            # if col < cols : 
+            #     ret = recurrence(row, rows, col + 1, cols, temp, ret)
+            # if row < rows:
+            #     ret = recurrence(row + 1, rows, col, col + 1, temp, ret)
+            
+            # return ret
+
+        # ret = 0
+        # result = []
+        # temp = []
+        # ret  = recurrence(0, n - 1, 0, m - 1, temp, ret)
+        # # print(result)
+
+        # return ret
+
+        def uniquePaths(self, m: int, n: int) -> int:
+            # ret = 0
+            # temp = [[1] * m] + [[1] + [0] * (m - 1) for _ in range(n - 1)]
+            # print(temp)
+            # print(temp[1][0])
+
+            # for col in range(1, n):
+            #     for row in range(1, m):
+            #         temp[col][row] = temp[col - 1][row] + temp[col][row - 1]
+            
+            # ret = temp[-1][-1]
+
+            # return ret 
+
+            # ret =  0 
+            # precols = [1] * m
+            # cols = [1] * m
+
+            # for _ in range(1, n):
+            #     for col in range(1, m):
+            #         cols[col] = cols[col - 1] + precols[col]
+            #     precols = cols[:] 
+
+            # ret = precols[-1]
+
+            # return ret 
+
+            ret =  0 
+            cols = [1] * m
+
+            for _ in range(1, n):
+                for col in range(1, m):
+                    cols[col] += cols[col - 1]
+            ret = cols[-1]
+
+            return ret 
+
+
+
 
 if __name__ == "__main__":
     solu = Solution()
