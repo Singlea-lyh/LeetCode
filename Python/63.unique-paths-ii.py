486eaefd760bd3c344491cc555df56eaf2180cfb
Singlea-lyh <chinese_lyh@163.com>
2020-06-28 18:05:05 +0800

    LeetCode-63

--- Python/63.unique-paths-ii.py
+++ Python/63.unique-paths-ii.py
@@ -50,15 +50,64 @@
 
 # @lc code=start
 class Solution:
-    def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
-        ret = 0 
+    def uniquePathsWithObstacles(self, obstacleGrid: list) -> int:
+        # ret = 0 
+        # if not obstacleGrid:
+        #     return ret
+        # rowlen = len(obstacleGrid)
+        # colulen = len(obstacleGrid[0])
 
+        # obstaclelist = [[0] * colulen for _ in range(rowlen)]
+        # print(obstaclelist)
+
+        # for row in range(rowlen):
+        #     if obstacleGrid[row][0] == 1:
+        #         break
+        #     obstaclelist[row][0] = 1
+        # for colu in range(colulen):
+        #     if obstacleGrid[0][colu]:
+        #         break
+        #     obstaclelist[0][colu] = 1
+        # print(obstaclelist)
+
+        # for row in range(1, rowlen):
+        #     for colu in range(1, colulen):
+        #         if obstacleGrid[row][colu] == 1:
+        #             obstaclelist[row][colu] = 0
+        #         else:
+        #             obstaclelist[row][colu] = obstaclelist[row - 1][colu] + obstaclelist[row][colu - 1]
+        
+        # ret = obstaclelist[-1][-1]
+        # return ret
+
+        ret = 0 
+        if not obstacleGrid:
+            return ret
+        rowlen = len(obstacleGrid)
+        colulen = len(obstacleGrid[0])
+
+        prelist = [0 for _ in range(colulen)]
+        for colu in range(colulen):
+            if obstacleGrid[0][colu] == 1:
+                break
+            prelist[colu] = 1
+        for row in range(1, rowlen):
+            for colu in range(colulen):
+                if obstacleGrid[row][colu] == 1:
+                    prelist[colu] = 0
+                else:
+                    if colu > 0: 
+                        prelist[colu] += prelist[colu - 1]
+                    else:
+                        prelist[colu] = prelist[colu]
+        ret = prelist[-1]
 
         return ret
+                 
 
 if __name__ == "__main__":
     solu = Solution()
-    obstacles = [[0,0,0],[0,1,0],[0,0,0]]
+    obstacles =[[1],[0]]
 
     ret = solu.uniquePathsWithObstacles(obstacles)
 
---
+++ Python/64.minimum-path-sum.py
@@ -0,0 +1,53 @@
+#
+# @lc app=leetcode id=64 lang=python3
+#
+# [64] Minimum Path Sum
+#
+# https://leetcode.com/problems/minimum-path-sum/description/
+#
+# algorithms
+# Medium (51.36%)
+# Likes:    2965
+# Dislikes: 61
+# Total Accepted:    415K
+# Total Submissions: 770.2K
+# Testcase Example:  '[[1,3,1],[1,5,1],[4,2,1]]'
+#
+# Given a m x n grid filled with non-negative numbers, find a path from top
+# left to bottom right which minimizes the sum of all numbers along its path.
+# 
+# Note: You can only move either down or right at any point in time.
+# 
+# Example:
+# 
+# 
+# Input:
+# [
+# [1,3,1],
+# ⁠ [1,5,1],
+# ⁠ [4,2,1]
+# ]
+# Output: 7
+# Explanation: Because the path 1→3→1→1→1 minimizes the sum.
+# 
+# 
+#
+
+# @lc code=start
+class Solution:
+    def minPathSum(self, grid: list) -> int:
+        ret = 0
+
+
+        print(ret)
+
+if __name__ == "__main__":
+    solu = Solution()
+    grid = [[1,3,1],⁠ [1,5,1],⁠ [4,2,1]]
+
+    ret = solu.minPathSum(grid)
+
+    print(ret)
+        
+# @lc code=end
+
