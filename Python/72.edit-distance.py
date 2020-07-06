cbd3085fec819571c68fefc6664d0ff02abfcdb8
Singlea-lyh <chinese_lyh@163.com>
2020-07-06 18:30:48 +0800

    LeetCode-72

--- Python/72.edit-distance.py
+++ Python/72.edit-distance.py
@@ -53,16 +53,56 @@
 # @lc code=start
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
-        ret = 0
+        # ret = 0
+        # word1len = len(word1)
+        # word2len = len(word2)
 
+        # dp = [[0] * (word2len + 1) for _ in range(word1len + 1)]
+       
+        # dp[0][0] = 0
 
-        return ret
+        # for colu in range(1, word2len + 1): 
+        #     dp[0][colu] = colu
+        # for row in range(1, word1len + 1):
+        #     dp[row][0] = row
+        # # print(dp)
+
+        # for row in range(1, word1len + 1):
+        #     for colu in range(1, word2len + 1):
+        #         left = dp[row][colu - 1] + 1
+        #         down = dp[row - 1][colu] + 1
+        #         left_down = dp[row - 1][colu - 1]
+        #         if word1[row - 1] == word2[colu - 1]:
+        #            left_down = dp[row - 1][colu  -1]
+        #         else:
+        #             left_down = dp[row - 1][colu - 1] + 1
+        #         dp[row][colu] = min(min(left, down), left_down)
+            
+        # # print(dp)
+        # ret = dp[-1][-1]
+
+              
+        # return ret
+
+        import functools
+        @functools.lru_cache(None)
+        def helper(i, j):
+            if i == len(word1) or j == len(word2):
+                return len(word1) - i + len(word2) - j
+            if word1[i] == word2[j]:
+                return helper(i + 1, j + 1)
+            else:
+                inserted = helper(i, j + 1)
+                deleted = helper(i + 1, j)
+                replaced = helper(i + 1, j + 1)
+                return min(inserted, deleted, replaced) + 1
+        return helper(0, 0)
 
 
 if __name__ == "__main__":
     solu = Solution()
-    word1 = ""
-    word2 = ""
+    word1 = "horse"
+    word2 = "ros"
     ret = solu.minDistance(word1, word2)
 
     print(ret)
