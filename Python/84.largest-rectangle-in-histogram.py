1268c52daf92d3d78ef74c9255a28a4ed5d5bb12
Singlea-lyh <chinese_lyh@163.com>
2020-07-28 22:45:59 +0800

    LeetCode-87

--- Python/84.largest-rectangle-in-histogram.py
+++ Python/84.largest-rectangle-in-histogram.py
@@ -45,13 +45,96 @@ from typing import List
 
 class Solution:
     def largestRectangleArea(self, heights: List[int]) -> int:
-        ret = 0
+        # ret = 0
+        # heightslen = len(heights)
+        # maxrect = 0
+        # import sys
+        # minheight = sys.maxsize
 
-        return ret
+        # if heightslen == 0:
+        #     return ret
+        
+        # start = heightslen // 2 - 1
+        # end = (heightslen // 2)
+        # maxrect = heights[start] if heights[start] > heights[end] else heights[end]
+        # minheight = heights[start] if heights[start] < heights[end] else heights[end]
+        # maxrect = (minheight * (end - start + 1)) if (minheight * (end - start + 1)) > maxrect else maxrect
+
+        # while start > 0 or end < heightslen - 1:
+        #     if start == 0:
+        #         end = end + 1
+        #         minheight = heights[end] if heights[end] < minheight else minheight
+        #         maxrect = maxrect if maxrect > heights[end] else heights[end]
+        #     elif end == heightslen - 1:
+        #         start = start - 1
+        #         minheight = heights[start] if heights[start] < minheight else minheight
+        #         maxrect = maxrect if heights[start] < maxrect else heights[start]
+        #     else:
+        #         if heights[start -  1] >= heights[end + 1]:
+        #             start = start - 1
+        #             minheight = heights[start] if heights[start] < minheight else minheight
+        #             maxrect = maxrect if heights[start] < maxrect else heights[start]
+        #         else:
+        #             end = end + 1
+        #             minheight = heights[end] if heights[end] < minheight else minheight
+        #             maxrect = maxrect if maxrect > heights[end] else heights[end]
+                
+        #         maxrect = maxrect if maxrect > (minheight * (end -start + 1)) else (minheight * (end -start + 1))
+        
+        # ret = maxrect
+
+        # return ret
+
+        # ret = 0 
+        # heightslen = len(heights)
+        # maxrect = 0
+        # if heightslen == 0:
+        #     return ret
+        
+        # maxrectarray = [[0 for _ in range(heightslen)] for _ in range(heightslen)]
+        # minheightarray = [[0 for _ in range(heightslen)] for _ in range(heightslen)]
+        # print(maxrectarray)
+
+        # for i in range(heightslen):
+        #     maxrectarray[i][i] = heights[i]
+        #     minheightarray[i][i] = heights[i]
+        #     maxrect = maxrect if maxrect > heights[i] else heights[i]
+        # print(maxrectarray)
+        
+        # for i in range(1, heightslen):
+        #     for row in range(heightslen - i):
+        #         minheightarray[row][row + i] = minheightarray[row][row + i - 1] if minheightarray[row][row + i - 1] <= heights[row + i] else heights[row + i]
+        #         maxtemp = maxrectarray[row][row + i - 1] if maxrectarray[row][row - 1] else maxrectarray[row + 1][row + i] 
+        #         temp = minheightarray[row][row + i] * (i + 1)
+        #         # print(temp)
+        #         maxrectarray[row][row + i] = maxtemp if maxtemp > temp else temp
+        #         maxrect = maxrect if maxrect > maxrectarray[row][row + i] else maxrectarray[row][row + i]
+        
+        # ret = maxrect
+
+        # return ret
+
+
+        ret = 0
+        heightslen = len(heights)
+
+        heights = [0] + heights + [0]
+        size = heightslen + 2
+
+        stack = [0]
+
+        for i in range(1, size):
+            while heights[i] < heights[stack[-1]]:
+                cur_height = heights[stack.pop()]
+                cur_width = i - stack[-1] - 1
+                ret = max(ret, cur_height * cur_width)
+            stack.append(i)
+
+        return ret 
 
 if __name__ == "__main__":
     solu = Solution()
-    heights = [2,1,5,6,2,3]
+    heights = [4,4,5,4,1,3]
     ret = solu.largestRectangleArea(heights)
     print(ret)
         
