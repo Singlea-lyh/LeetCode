6e2cba827e069f379bcc43a9a273c7c151de7e82
Singlea-lyh <chinese_lyh@163.com>
2020-07-30 22:46:20 +0800

    LeetCode-85

--- Python/85.maximal-rectangle.py
+++ Python/85.maximal-rectangle.py
@@ -36,15 +36,115 @@ from typing import List
 
 class Solution:
     def maximalRectangle(self, matrix: List[List[str]]) -> int:
+        # ret = 0
+        # maxmal = 0
+        # rows = len(matrix)
+        # if rows == 0:
+        #     return ret
+        # colus = len(matrix[0])
+
+        # maxmatrix = [[0 for _ in range(colus)] for _ in range(rows)]
+        # print(maxmatrix)
+
+        # for row in range(rows):
+        #     for col in range(colus):
+        #         if matrix[row][col] == "1":
+        #             if col == 0:
+        #                 maxmatrix[row][col] = 1
+        #             else:
+        #                 maxmatrix[row][col] = maxmatrix[row][col - 1] + 1
+        #         else:
+        #             maxmatrix[row][col] = 0
+        
+        #         minwidth = maxmatrix[row][col]
+
+        #         for cur_row in range(row, -1, -1):
+        #             height = row - cur_row + 1
+        #             minwidth = min(minwidth, maxmatrix[cur_row][col])
+        #             maxmal = max(maxmal, height * minwidth)
+
+        # ret = maxmal
+                
+        # return ret
+
+        # def largestrectangle(heights):
+        #     maxrect = 0
+        #     heightslen = len(heights)
+
+        #     stack = [0]
+        #     heights = [0] + heights + [0]
+        #     size = heightslen + 2
+
+        #     for col in range(1, size):
+        #         while heights[col] < heights[stack[-1]]:
+        #             height = heights[stack.pop()]
+        #             length = col - stack[-1] - 1
+        #             maxrect = max(maxrect, height * length)
+        #         stack.append(col)
+        #     return maxrect
+
+        # ret = 0
+        # maxret = 0
+        # rows = len(matrix)
+        # if rows == 0:
+        #     return ret
+        
+        # colus = len(matrix[0])
+        # rowheights = [0 for _ in range(colus)]
+
+        # for row in range(rows):
+        #     for colu in range(colus):
+        #         if matrix[row][colu] == "1":
+        #             rowheights[colu] += 1
+        #         else:
+        #             rowheights[colu] = 0
+        #     maxret = max(maxret, largestrectangle(rowheights))
+        # ret = maxret
+        # return ret
         ret = 0
+        maxret = 0
         rows = len(matrix)
         if rows == 0:
             return ret
+        
         colus = len(matrix[0])
+        rowheights = [0 for _ in range(colus)]
+        leftless = [-1 for _ in range(colus)]
+        rightless = [colus for _ in range(colus)]
 
+        for row in range(rows):
+            for colu in range(colus):
+                if matrix[row][colu] == "1":
+                    rowheights[colu] += 1
+                else:
+                    rowheights[colu] = 0
+
+            bound = -1 
+            for colu in range(colus):
+                if matrix[row][colu] == "1":
+                    leftless[colu] = max(leftless[colu], bound)
+                else:
+                    leftless[colu] = -1
+                    bound = colu
+
+            bound = colus
+            for colu in range(colus - 1, -1, -1): 
+                if matrix[row][colu] == "1":
+                    rightless[colu] = min(rightless[colu], bound)
+                else:
+                    rightless[colu] = colus
+                    bound = colu
+            print(matrix[row])
+            print(leftless)
+            print(rightless)
+            
+            for colu in range(colus -  1, -1 , -1):
+                maxret = max(maxret, (rightless[colu] - leftless[colu] -  1) * rowheights[colu]) 
+        
+        ret = maxret
 
         return ret
-
+        
 if __name__ == "__main__":
     solu = Solution()
     matrix = [ ["1","0","1","0","0"],
@@ -52,6 +152,7 @@ if __name__ == "__main__":
                ["1","1","1","1","1"],
                ["1","0","0","1","0"]
             ]
+    # matrix = [["1","0"]]
     ret = solu.maximalRectangle(matrix)
     print(ret)
         
